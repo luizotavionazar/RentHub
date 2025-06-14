c6ab0ed61ed7a4dfe4402aa41690854ccb708152
jhonnie <jhonniegabriellg@gmail.com>
2025-06-14 11:48:37 -0300

    simulação e teste de login

---
+++ RentHub/src/main/java/br/edu/iftm/renthub/model/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.iftm.renthub.model;
+
+/**
+ *
+ * @author jhonn
+ */
+public class Usuario {
+    private String usuario;
+    private char[] senha;
+    
+    public Usuario(String usuario, char[] senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public char[] getSenha() {
+        return senha;
+    }
+
+    public void setSenha(char[] senha) {
+        this.senha = senha;
+    }
+    
+    
+}
