ecb6daccbc9e66fd8a0dca01cc6989beb8254c17
Luiz Otavio <luizzotavionazar@outlook.com>
2025-07-17 21:13:58 -0300

    continuação

--- RentHub/src/main/java/br/edu/iftm/renthub/dao/TotalizacaoDAO.java
+++ RentHub/src/main/java/br/edu/iftm/renthub/dao/TotalizacaoDAO.java
@@ -1,48 +1,58 @@
 package br.edu.iftm.renthub.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
+import br.edu.iftm.renthub.control.TotalizacaoController;
+import br.edu.iftm.renthub.model.Contrato;
 import br.edu.iftm.renthub.model.Totalizacao;
 import br.edu.iftm.renthub.view.RegistrosLog;
 
 public class TotalizacaoDAO {
     private final Connection conexaoBanco;
+    private TotalizacaoController totalizacaoController;
 
     public TotalizacaoDAO(Connection conexao) {
         this.conexaoBanco = conexao;
+        this.totalizacaoController = new TotalizacaoController(conexao);
     }
 
     RegistrosLog log = new RegistrosLog();
-    
-    public String Cadastrartotalizacao (int contrato, double valor, double multa, double juros, double total) {
-        String sql = "INSERT INTO totalizacao (id_contrato, valor, multa, juros, vlr_total) VALUES (?, ?, ?, ?, ?)";
-        try (Connection conn = ConexaoDAO.conexaoBd();){
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, contrato);
+
+    public boolean totalizar(Integer idContrato, double valor, double juros, double multa) {
+        String sql = "INSERT INTO totalizacao (id_contrato, valor, juros, multa) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = conexaoBanco.prepareStatement(sql)) {
+            stmt.setInt(1, idContrato);
             stmt.setDouble(2, valor);
             stmt.setDouble(3, multa);
             stmt.setDouble(4, juros);
-            stmt.setDouble(5, total);
             stmt.executeUpdate();   
-            return "Totalização realizada com sucesso!\n";
+            return true;
         } catch (SQLException e) {
-            return "Erro ao realizar a Totalização";
+            return false;
         }
     }
 
-    public Totalizacao buscarTotalizacao (int contrato) {
-        String sql = "SELECT * FROM totalizacao where id_contrato = ?";
-        try (Connection conn = ConexaoDAO.conexaoBd();){
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, contrato);
+    public Totalizacao buscarPorContrato(Integer idContrato) {
+        String sql = "SELECT id, valor, juros, multa FROM totalizacao where id_contrato = ?";
+        try (PreparedStatement stmt = conexaoBanco.prepareStatement(sql)){
+            stmt.setInt(1, idContrato);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
-                //return new Totalizacao(rs.getInt("id_tot"), rs.getInt("id_contrato"), rs.getDouble("valor"), rs.getDouble("multa"), rs.getDouble("juros"), rs.getDouble("vlr_total"));
-            }
-            return null;
+                Totalizacao totalizacao = new Totalizacao(rs.getInt("id"), 
+                                                          idContrato,
+                                                          rs.getDouble("valor"),
+                                                          rs.getDouble("juros"),
+                                                          rs.getDouble("multa"),
+                                                          totalizacaoController.calcularTotal(rs.getDouble("valor"), rs.getDouble("multa"), rs.getDouble("juros")));
+            return totalizacao;
+            } else {
+                return null;
+            }            
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
