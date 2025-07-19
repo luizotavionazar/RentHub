5dcaf2375e83598eeafbd22f334c298a6be4749f
Luiz Otavio <luizzotavionazar@outlook.com>
2025-07-19 16:48:57 -0300

    ajustes no cadastro de usuario

--- RentHub/src/main/java/br/edu/iftm/renthub/dao/UsuarioDAO.java
+++ RentHub/src/main/java/br/edu/iftm/renthub/dao/UsuarioDAO.java
@@ -16,7 +16,7 @@ public class UsuarioDAO {
     RegistrosLog log = new RegistrosLog();
 
     public boolean cadastrar(String nomeUsuario, String senhaCrip) throws SQLException {
-        log.registrarLog(1, "UsuarioDAO", "cadastrar", "", "Cadastrando o usuário no Banco de Dados");
+        log.registrarLog(1, "UsuarioDAO", "cadastrar", "usuario", "Cadastrando o usuário no Banco de Dados");
         StringBuilder query = new StringBuilder();
         query.append("INSERT INTO usuario (nome, senha) ");
         query.append("VALUES (?, ?) ");
@@ -24,17 +24,17 @@ public class UsuarioDAO {
             stmt.setString(1, nomeUsuario);
             stmt.setString(2, senhaCrip);
             stmt.executeUpdate();
-            log.registrarLog(2, "UsuarioDAO", "cadastrar", "", "Usuário cadastrado no Banco de Dados com sucesso");
+            log.registrarLog(2, "UsuarioDAO", "cadastrar", "usuario", "Usuário cadastrado no Banco de Dados com sucesso");
             return true;
         } catch (SQLException e) {
-            log.registrarLog(4, "UsuarioDAO", "cadastrar", "", "Erro ao cadastrar o usuário no Banco de Dados: " + e.getMessage());
+            log.registrarLog(4, "UsuarioDAO", "cadastrar", "usuario", "Erro ao cadastrar o usuário no Banco de Dados: " + e.getMessage());
             e.printStackTrace();
             return false;
         }
     }
 
     public String buscarSenha(String nomeUsuario) throws SQLException {
-        log.registrarLog(1, "UsuarioDAO", "buscarSenha", "", "Buscando a senha do usuário no Banco de Dados");
+        log.registrarLog(1, "UsuarioDAO", "buscarSenha", "usuario", "Buscando a senha do usuário no Banco de Dados");
         StringBuilder query = new StringBuilder();
         query.append("SELECT senha ");
         query.append("FROM usuario ");
@@ -45,11 +45,34 @@ public class UsuarioDAO {
             if (rs.next()) {
                 return rs.getString("senha");
             } else {
-                log.registrarLog(3, "UsuarioDAO", "buscarSenha", "", "Usuário não está cadastrado no Banco de Dados");
+                log.registrarLog(3, "UsuarioDAO", "buscarSenha", "usuario", "Não foi possivel encontrar a senha do usuário no Banco de Dados");
                 return null;
             }
         } catch (SQLException e) {
-            log.registrarLog(4, "UsuarioDAO", "buscarSenha", "", "Erro ao buscar a senha do usuário no Banco de Dados: " + e.getMessage());
+            log.registrarLog(4, "UsuarioDAO", "buscarSenha", "usuario", "Erro ao buscar a senha do usuário no Banco de Dados: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String buscarNome(String nomeUsuario) throws SQLException {
+        log.registrarLog(1, "UsuarioDAO", "buscarUsuario", "usuario", "Buscando o usuário no Banco de Dados");
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT nome ");
+        query.append("FROM usuario ");
+        query.append("WHERE nome = ?");
+        try (PreparedStatement stmt = conexaoBanco.prepareStatement(query.toString())) {
+            stmt.setString(1, nomeUsuario);
+            var rs = stmt.executeQuery();
+            if (rs.next()) {
+                log.registrarLog(2, "UsuarioDAO", "buscarUsuario", "usuario", "Usuário localizado no Banco de Dados");
+                return rs.getString("nome");
+            } else {
+                log.registrarLog(3, "UsuarioDAO", "buscarUsuario", "usuario", "Usuário não está cadastrado no Banco de Dados");
+                return null;
+            }
+        } catch (SQLException e) {
+            log.registrarLog(4, "UsuarioDAO", "buscarUsuario", "usuario", "Erro ao buscar o usuário no Banco de Dados: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
